195f8d4d0469556b71916ecca95b5084bad6501b
alessandro baiocco <baioccoalessandro97@gmail.com>
2023-10-03 17:00:34 +0200

    ok adesso è perfetto

--- Esercizio1IfElse.java
+++ Esercizio1IfElse.java
@@ -60,7 +60,7 @@ public class Esercizio1IfElse {
 
     private static boolean annoBisestile(int a) {
 
-        if (a % 4 == 0 && (a % 100 == 0 && a % 400 == 0)) {
+        if (a % 100 == 0 ? a % 400 == 0 : a % 4 == 0) {
             return true;
         } else {
             return false;
